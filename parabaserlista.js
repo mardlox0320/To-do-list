929841add5e4324d37ebac3f616d95cebc9c55a7
mardlox0320 <tereza.rocha.3@gmail.com>
2025-10-18 14:16:27 -0300

    Add files via upload

---
+++ parabaserlista.js
@@ -0,0 +1,43 @@
+const armazenarkey = 'tarefas';
+const ovo = document.getElementById('coiso');
+let tarefaInput = document.getElementById('tarefaInput');
+const lista = document.getElementById('lista');
+const troço = document.addEventListener('keydown', function(event) {
+
+    if (event.key === 'Enter') {
+        adicionarTarefa()
+        show();
+    }
+});
+
+function adicionarTarefa() {
+    if (tarefaInput.value == '') {
+        alert('Digite uma tarefa');
+    }
+    else {
+         let values = JSON.parse(localStorage.getItem(armazenarkey) || '[]');
+        values.push({
+            name: tarefaInput.value
+        });
+        localStorage.setItem(armazenarkey, JSON.stringify(values));
+        show();
+        tarefaInput.value = '';
+    }
+}
+    const show = () => {
+        let values = JSON.parse(localStorage.getItem(armazenarkey) || '[]'); 
+        lista.innerHTML = ''
+        let i = 0
+        while(i < values.length)  {
+            lista.innerHTML += `<li>${values[i]['name']} <button type="reset" onclick="remove('${values[i]['name']}')">t</button></li>`
+            i++
+        }  
+    }
+    const remove = (indice) => {
+        let values = JSON.parse(localStorage.getItem(armazenarkey) || '[]');
+       let index = values.findIndex(x => x.name == indice);
+       values.splice(index, 1)
+        localStorage.setItem(armazenarkey, JSON.stringify(values));
+        show()
+    }
+    show()
